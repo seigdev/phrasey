0a342ba5edf73f6c339011de649a916a8ee2fe0b
Moyosoore Okeyode <sooreoluwwaa@gmail.com>
2025-02-20 08:19:00 +0000

    added copy to clipboard function

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "786789033"
+    serviceWorkerVersion: "4146948666"
   }
 });
